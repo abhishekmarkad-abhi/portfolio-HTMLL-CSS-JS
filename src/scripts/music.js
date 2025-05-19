bb1fd217d20d8b83004ee81351ca23578c6c3f2c
abhishekmarkad-abhi <abhishekmarkad33@gmail.com>
2025-05-19 14:35:37 +0530

    add

--- src/scripts/music.js
+++ src/scripts/music.js
@@ -1,23 +1,17 @@
-// let obj ={
-//   name:"ketan",
-//   age :21,
-//   subjects:["java","python","adv java"],
-//   marks:{
-//       java:80,
-//       python:90,
-//   },
-//   getUser:null
-// }
-// // console.log(obj)
-
-// let array = [
-//   "Abhishek",8,9,true,null,{name:"Vivek"}
-// ]
-// console.log(array[5].name)
-
-function myRun(){
-  return 2
-}
-let data = myRun();
-
-console.log(data)
+    function FetchData(url, success, failure)
+      {
+          if(url==="fakestore"){
+              success('Response data received');
+          } else {
+              failure('Unable to Fetch Data - Invalid URL');
+          }
+      }
+      FetchData(
+          prompt("Enter URL"),
+          function(response) {
+             document.write(response);
+          },
+          function(error){
+             document.write(error);
+          }
+      )
